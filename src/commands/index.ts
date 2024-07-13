07e295342f641b0d814ac0db40bcebf438eee45c
Alix BESSON <alix.besson@doxallia.com>
2024-07-13 09:49:28 +0200

    move ping.ts and fix import in commands/index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,1 +1,1 @@
-import "./src/commands/ping";
+import "./utils/ping";
--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,12 +1,12 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-import CommandsUtils from "../utils/commands/commandsUtils";
+import CommandsUtils from "../../utils/commands/commandsUtils";
 
 CommandsUtils.add({
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   execute: async (interaction) => {
-    await(interaction as ChatInputCommandInteraction).reply(
+    await (interaction as ChatInputCommandInteraction).reply(
       `Pong! Latence: \`${Date.now() - interaction.createdTimestamp}ms\``
     );
   },
